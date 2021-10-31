fd30af75e12e654138c4c27ae6c9450a61b88e9f
titabash <titabash0819@gmail.com>
2021-10-31 13:49:41 +0900

    プロジェクト分離準備

--- controller/__init__.py
+++ controller/__init__.py
@@ -9,3 +9,8 @@ def create_app():
     # app.config.from_object(config_object)
     app.register_blueprint(hello_controller.blueprint)
     return app
+
+
+def excute_batch():
+    result = hello_controller.main_api_controller()
+    return result
